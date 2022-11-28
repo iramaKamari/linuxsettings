78244df05f50bef42a5a18092e635354f2d98996
iramakamari <tobias.grundel@ericsson.com>
2022-11-28 10:12:10 +0100

    Zsh, use ~/ instead of fix path for terminal background

--- zshrc/lib/terminal_background_image.zsh
+++ zshrc/lib/terminal_background_image.zsh
@@ -1,4 +1,4 @@
-fd -t f . /home/tobias/Documents/wallpapers/ > wallpapers
+fd -t f . ~/Documents/wallpapers/ > wallpapers
 num_wallpapers="$(wc -l < wallpapers)"
 wallpaper="$(shuf -i 1-$num_wallpapers -n 1)"
 ln -sf "$(sed -n "$wallpaper p" wallpapers)" ~/Documents/wallpapers/terminal_bg.png
