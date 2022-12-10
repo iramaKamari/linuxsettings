f118049bf6163ff0b6aaa1746b6d2e3f3a9f9ec7
Tobias Grundel <tobias.grundel@hotmail.com>
2022-12-10 01:13:15 +0100

    [ZSHRC] First change for colored man pages

--- zshrc/lib/colored_man_pages.zsh
+++ zshrc/lib/colored_man_pages.zsh
@@ -10,16 +10,51 @@
 # us       start underline
 # ue       stop underline
 
-function man() {
-	env \
-		LESS_TERMCAP_mb=$(printf "\e[1;33m") \
-		LESS_TERMCAP_md=$(printf "\e[1;33m") \
-		LESS_TERMCAP_me=$(printf "\e[0m") \
-		LESS_TERMCAP_se=$(printf "\e[0m") \
-		LESS_TERMCAP_so=$(printf "\e[01;37m") \
-		LESS_TERMCAP_ue=$(printf "\e[0m") \
-		LESS_TERMCAP_us=$(printf "\e[1;4;32m") \
-		PAGER="${commands[less]:-$PAGER}" \
-		PATH="$HOME/bin:$PATH" \
-			man "$@"
+# Requires colors autoload.
+# See termcap(5).
+
+# Set up once, and then reuse. This way it supports user overrides after the
+# plugin is loaded.
+typeset -AHg less_termcap
+
+# bold & blinking mode
+less_termcap[mb]="${fg_bold[red]}${bg[black]}"
+less_termcap[md]="${fg_bold[red]}${bg[black]}"
+less_termcap[me]="${reset_color}"
+# standout mode
+less_termcap[so]="${fg_bold[yellow]}${bg[black]}"
+less_termcap[se]="${reset_color}"
+# underlining
+less_termcap[us]="${fg_bold[green]}${bg[black]}"
+less_termcap[ue]="${reset_color}"
+
+# Handle $0 according to the standard:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# Absolute path to this file's directory.
+typeset -g __colored_man_pages_dir="${0:A:h}"
+
+function colored() {
+  local -a environment
+
+  # Convert associative array to plain array of NAME=VALUE items.
+  local k v
+  for k v in "${(@kv)less_termcap}"; do
+    environment+=( "LESS_TERMCAP_${k}=${v}" )
+  done
+
+  # Prefer `less` whenever available, since we specifically configured
+  # environment for it.
+  environment+=( PAGER="${commands[less]:-$PAGER}" )
+
+  command env $environment "$@"
+}
+
+# Colorize man and dman/debman (from debian-goodies)
+function man \
+  dman \
+  debman {
+  colored $0 "$@"
 }
