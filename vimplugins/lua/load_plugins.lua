5da46b5ca7b279245665c2774f389a342fee6517
Tobias Grundel <tobias.grundel@hotmail.com>
2022-12-18 23:40:39 +0100

    [NVIM] Limit characters for cmp and add icons

--- vimplugins/lua/load_plugins.lua
+++ vimplugins/lua/load_plugins.lua
@@ -11,6 +11,7 @@ require "paq" {
   -- Completion
   "hrsh7th/nvim-cmp";
   "hrsh7th/cmp-nvim-lsp";
+  "onsails/lspkind.nvim";
   "hrsh7th/cmp-buffer";
   "hrsh7th/cmp-path";
   "hrsh7th/cmp-cmdline";
