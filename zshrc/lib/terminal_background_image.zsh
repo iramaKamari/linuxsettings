072a5468c80b6c040d6cb456636b6dd24030cc46
Tobias Grundel <tobias.grundel@hotmail.com>
2022-12-27 18:11:58 +0100

    [ZSH] List only .png files to set as terminal background

--- zshrc/lib/terminal_background_image.zsh
+++ zshrc/lib/terminal_background_image.zsh
@@ -1,4 +1,4 @@
-fd -t f . ~/Documents/wallpapers/ > wallpapers
+fd -t f "png$" . ~/Documents/wallpapers/ > wallpapers
 num_wallpapers="$(wc -l < wallpapers)"
 wallpaper="$(shuf -i 1-$num_wallpapers -n 1)"
 ln -sf "$(sed -n "$wallpaper p" wallpapers)" ~/Documents/wallpapers/terminal_bg.png
